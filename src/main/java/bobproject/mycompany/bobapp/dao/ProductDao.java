7e25995cfbc1429a1004757d39a53449f783080b
changwoo <cwoo509@naver.com>
2021-01-28 11:06:22 +0900

    창우 커밋

--- src/main/java/bobproject/mycompany/bobapp/dao/ProductDao.java
+++ src/main/java/bobproject/mycompany/bobapp/dao/ProductDao.java
@@ -5,12 +5,21 @@ import javax.annotation.Resource;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 
-@Resource
+import bobproject.mycompany.bobapp.dto.Product;
+
+@Repository
 public class ProductDao {
 	private static final Logger logger 
 	= LoggerFactory.getLogger(ProductDao.class);
 	
 	@Resource
 	private SqlSessionTemplate sst;
+
+	public int insert(Product product) {
+		int rows = sst.insert("products.insert", product);
+		return rows;
+	}
+
 }
--- src/main/java/bobproject/mycompany/bobapp/dto/Product.java
+++ src/main/java/bobproject/mycompany/bobapp/dto/Product.java
@@ -1,18 +1,105 @@
 package bobproject.mycompany.bobapp.dto;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class Product {
 	private int pno;
 	private String pname;
-	private String pmainimg;
-	private int pprice;
 	private String pamount;
+	private String pcontent;
+	private String psubtitle;
+	private String pdifficulty;
+
+	private String pmainimg;
 	private String psubimg1;
 	private String psubimg2;
-	private int pweight;
-	private int ptime;
-	private String psubtitle;
+	
+	private int pprice =0;
+	private int pweight =0;
+	private int ptime =0;
 	private String pmainimgtype;
 	private String psubimg1type;
+	private String psubimg2type;
+	
+	private MultipartFile pattach1;
+	private MultipartFile pattach2;
+	private MultipartFile pattach3;
+	
+	
+	
+	
+	public int getPno() {
+		return pno;
+	}
+	public void setPno(int pno) {
+		this.pno = pno;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getPamount() {
+		return pamount;
+	}
+	public void setPamount(String pamount) {
+		this.pamount = pamount;
+	}
+	public String getPcontent() {
+		return pcontent;
+	}
+	public void setPcontent(String pcontent) {
+		this.pcontent = pcontent;
+	}
+	public String getPsubtitle() {
+		return psubtitle;
+	}
+	public void setPsubtitle(String psubtitle) {
+		this.psubtitle = psubtitle;
+	}
+	public String getPdifficulty() {
+		return pdifficulty;
+	}
+	public void setPdifficulty(String pdifficulty) {
+		this.pdifficulty = pdifficulty;
+	}
+	public String getPmainimg() {
+		return pmainimg;
+	}
+	public void setPmainimg(String pmainimg) {
+		this.pmainimg = pmainimg;
+	}
+	public String getPsubimg1() {
+		return psubimg1;
+	}
+	public void setPsubimg1(String psubimg1) {
+		this.psubimg1 = psubimg1;
+	}
+	public String getPsubimg2() {
+		return psubimg2;
+	}
+	public void setPsubimg2(String psubimg2) {
+		this.psubimg2 = psubimg2;
+	}
+	public int getPprice() {
+		return pprice;
+	}
+	public void setPprice(int pprice) {
+		this.pprice = pprice;
+	}
+	public int getPweight() {
+		return pweight;
+	}
+	public void setPweight(int pweight) {
+		this.pweight = pweight;
+	}
+	public int getPtime() {
+		return ptime;
+	}
+	public void setPtime(int ptime) {
+		this.ptime = ptime;
+	}
 	public String getPmainimgtype() {
 		return pmainimgtype;
 	}
@@ -31,80 +118,24 @@ public class Product {
 	public void setPsubimg2type(String psubimg2type) {
 		this.psubimg2type = psubimg2type;
 	}
-	private String psubimg2type;
+	public MultipartFile getPattach1() {
+		return pattach1;
+	}
+	public void setPattach1(MultipartFile pattach1) {
+		this.pattach1 = pattach1;
+	}
+	public MultipartFile getPattach2() {
+		return pattach2;
+	}
+	public void setPattach2(MultipartFile pattach2) {
+		this.pattach2 = pattach2;
+	}
+	public MultipartFile getPattach3() {
+		return pattach3;
+	}
+	public void setPattach3(MultipartFile pattach3) {
+		this.pattach3 = pattach3;
+	}
 	
-	public int getPno() {
-		return pno;
-	}
-	public void setPno(int pno) {
-		this.pno = pno;
-	}
-	public String getPname() {
-		return pname;
-	}
-	public void setPname(String pname) {
-		this.pname = pname;
-	}
-	public String getPmainimg() {
-		return pmainimg;
-	}
-	public void setPmainimg(String pmainimg) {
-		this.pmainimg = pmainimg;
-	}
-	public int getPprice() {
-		return pprice;
-	}
-	public void setPprice(int pprice) {
-		this.pprice = pprice;
-	}
-	public String getPamount() {
-		return pamount;
-	}
-	public void setPamount(String pamount) {
-		this.pamount = pamount;
-	}
-	public String getPsubimg1() {
-		return psubimg1;
-	}
-	public void setPsubimg1(String psubimg1) {
-		this.psubimg1 = psubimg1;
-	}
-	public String getPsubimg2() {
-		return psubimg2;
-	}
-	public void setPsubimg2(String psubimg2) {
-		this.psubimg2 = psubimg2;
-	}
-	public int getPweight() {
-		return pweight;
-	}
-	public void setPweight(int pweight) {
-		this.pweight = pweight;
-	}
-	public int getPtime() {
-		return ptime;
-	}
-	public void setPtime(int ptime) {
-		this.ptime = ptime;
-	}
-	public String getPsubtitle() {
-		return psubtitle;
-	}
-	public void setPsubtitle(String psubtitle) {
-		this.psubtitle = psubtitle;
-	}
-	public String getPcontent() {
-		return pcontent;
-	}
-	public void setPcontent(String pcontent) {
-		this.pcontent = pcontent;
-	}
-	public String getPdifficulty() {
-		return pdifficulty;
-	}
-	public void setPdifficulty(String pdifficulty) {
-		this.pdifficulty = pdifficulty;
-	}
-	private String pcontent;
-	private String pdifficulty;
+	
 }
